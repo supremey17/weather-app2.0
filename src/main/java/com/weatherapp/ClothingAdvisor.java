14de38ee303554d6e6fd4457d3c5f34ce0be3e3e
saulvilla <svill2@brockport.edu>
2026-09-08 18:13:11 -0400

    Advice and slang now work in the settings and it remembers the last saved settings.

--- src/main/java/com/weatherapp/ClothingAdvisor.java
+++ src/main/java/com/weatherapp/ClothingAdvisor.java
@@ -5,24 +5,24 @@ public class ClothingAdvisor {
         StringBuilder advice = new StringBuilder();
 
         if(tempF < 40) {
-            advice.append("Gotta larp as a nyc hooligan bring a puffer Jacket");
+            advice.append("ITS MAD BRICK");
         } else if(tempF < 60){
             advice.append("Grab a light jacket it'll be warm ");
         } else if(tempF < 80){
-            advice.append("It's HOT bring something lighter than my hopes");
+            advice.append("Cook my kid in the car weather");
         }
 
         if (humidity > 70){
             advice.append("Wear some Linen or lightweight cotton to keep cool");
         }
         if (condition.equalsIgnoreCase("Rain") || condition.equalsIgnoreCase("Drizzle")) {
-            advice.append("Bring an umbrella. ");
+            advice.append("Raincoat ");
         } else if (condition.equalsIgnoreCase("Snow")) {
-            advice.append("Wear boots and a waterproof coat. ");
+            advice.append("Wear boots and a winter coat. ");
         }
 
         if (uvi >= 6) {
-            advice.append("High UV, wear sunscreen. Wear Light Colored Clothes, NO BLACK YOU'LL BURN ");
+            advice.append("High UV, If Sunscreen:Wear light-toned clothing, If no Sunscreen: wear dark colors");
         } else if (uvi >= 3) {
             advice.append("Moderate UV, sunscreen recommended. you do you i guess.   ");
         }
